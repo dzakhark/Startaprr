a81f1dde9274867b5f0a51607cc18ff868656049
Dmitriy Zakharchuk <dmitry.zakh1@gmail.com>
2018-02-16 00:02:00 +0300

    Added adaptive design

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,32 @@
 $(function() {
 
-	// Custom JS
+	$('.hamburger').click(function () {
+        $(this).toggleClass('is-active');
+        $('.nav').slideToggle(500);
+    });
 
+    checkVisibleItem('.team__list', '.team__item');
+    checkVisibleItem('.list__clients', '.client');
+
+    $(window).resize(function() {
+        checkVisibleItem('.team__list', '.team__item');
+        checkVisibleItem('.list__clients', '.client');
+    });
+
+    $('.top').click(function () {
+        console.log('top');
+        $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+    });
+
+    function checkVisibleItem(list, item) {
+        $(item).each(function () {
+            $(this).css({'display': 'none'});
+        });
+        var sliderSize = $(list).outerWidth();
+        var itemSize = $(item).outerWidth(true);
+        var count = Math.floor(sliderSize/itemSize);
+        for (var i = 0; i < count; i++) {
+            $(item).eq(i).css({'display' : 'block'})
+        }
+    }
 });
